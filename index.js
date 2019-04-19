ac8a54d0f3471ff722185bd0b48ed1846f361102
RAGAdox <rishitech882@gmail.com>
2019-04-19 08:23:15 +0530

    checking Done

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+require('dotenv').config()
 const express = require("express");
 const exphbs = require("express-handlebars");
 const fileUpload = require("express-fileupload");
 var bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-//require("./models/db");
-const testFolder = "./uploads/";
-const fs = require("fs");
-let filelist = [];
-let links = [];
-const linkDB = require('./models/links');
 const app = express();
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-mongoose.connect('mongodb+srv://RAGAdox:'+process.env.MONGO_ATLAS_PW+'@filejs-c1fms.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true });
+console.log('mongodb+srv://RAGAdox:'+process.env.MONGO_ATLAS_PW+'@filejs-c1fms.mongodb.net/LinkDB?retryWrites=true')
+mongoose.connect('mongodb+srv://RAGAdox:'+process.env.MONGO_ATLAS_PW+'@filejs-c1fms.mongodb.net/LinkDB?retryWrites=true',{ useNewUrlParser: true });
 
 app.use(fileUpload());
 
